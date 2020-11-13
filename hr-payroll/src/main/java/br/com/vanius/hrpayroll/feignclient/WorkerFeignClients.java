5659a8e2fe742de8c511c96ce6afaec35eb2e3d4
Vanius Oliveira <vaniusmax@gmail.com>
2020-11-13 13:34:12 -0300

    Ribbon load balance

--- hr-payroll/src/main/java/br/com/vanius/hrpayroll/feignclient/WorkerFeignClients.java
+++ hr-payroll/src/main/java/br/com/vanius/hrpayroll/feignclient/WorkerFeignClients.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import br.com.vanius.hrpayroll.entities.Worker;
 
 @Component
-@FeignClient(name = "hr-worker", url = "localhost:8001", path = "workers")
+@FeignClient(name = "hr-worker", path = "workers")
 public interface WorkerFeignClients {
 	
 	@GetMapping(value = "/{id}")
--- hr-worker/src/main/java/br/com/vanius/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/vanius/hrworker/resources/WorkerResource.java
@@ -2,12 +2,17 @@ package br.com.vanius.hrworker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 import br.com.vanius.hrworker.entities.Worker;
 import br.com.vanius.hrworker.repositories.WorkerRepository;
@@ -15,6 +20,11 @@ import br.com.vanius.hrworker.repositories.WorkerRepository;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+	
+	@Autowired
+	private Environment env;
+	
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 	
 	@Autowired
 	private WorkerRepository repo;
@@ -27,6 +37,9 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
+		
+		logger.info("PORT ="+ env.getProperty("local.server.port"));
+		
 		Worker obj = repo.findById(id).get();
 		return ResponseEntity.ok(obj);
 	}
