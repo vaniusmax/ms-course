018dc27d0d453901da30e1839d3ac72ecd8b2bc4
Vanius Oliveira <vaniusmax@gmail.com>
2020-11-16 15:08:32 -0300

    configuração dos clientes eureka

--- hr-worker/src/main/java/br/com/vanius/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/br/com/vanius/hrworker/HrWorkerApplication.java
@@ -2,7 +2,10 @@ package br.com.vanius.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.repository.cdi.Eager;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication {
 
