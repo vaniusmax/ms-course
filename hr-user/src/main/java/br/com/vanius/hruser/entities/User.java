f2c7719c3612251a2e5601406527a027f4c4e685
Vanius Oliveira <vaniusmax@gmail.com>
2020-11-19 18:16:50 -0300

    userRepository userResource zuul configuration

--- hr-user/src/main/java/br/com/vanius/hruser/entities/User.java
+++ hr-user/src/main/java/br/com/vanius/hruser/entities/User.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -24,8 +25,11 @@ public class User implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(unique = true)
 	private String email;
 	private String password;
+	
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), 
 	                                   inverseJoinColumns = @JoinColumn(name = "role_id"))
@@ -73,8 +77,10 @@ public class User implements Serializable {
 		this.password = password;
 	}
 	
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
+	
+
+	public Set<Role> getRoles() {
+		return roles;
 	}
 
 	@Override
