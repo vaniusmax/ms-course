867826b547fc08a7460f26cc5e0f60a24d25d412
Vanius Oliveira <vaniusmax@gmail.com>
2020-11-24 12:24:19 -0300

    security configs to config server

--- hr-oauth/src/main/java/br/com/vanius/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/br/com/vanius/hroauth/config/AppConfig.java
@@ -1,5 +1,6 @@
 package br.com.vanius.hroauth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,6 +9,9 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
+	
+	@Value("${jwt.secret}")
+	private String secret;
 	
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -19,7 +23,7 @@ public class AppConfig {
 		
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
 		
-		tokenConverter.setSigningKey("MY-SECRET-KEY");
+		tokenConverter.setSigningKey(secret);
 		
 		return tokenConverter;
 	}
